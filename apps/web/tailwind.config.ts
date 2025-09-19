f2c94fed5e9b4d8447863b647f7357913ceaccd7
Samuel Llibre-Pillco <dev@zyruks.com>
2025-09-19 16:34:59 -0400

    refactor: remove unused PostCSS and Tailwind configuration files

--- apps/web/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import basePresets from '@ui-guideline/tailwind-config';
-import type { Config } from 'tailwindcss';
-
-export default {
-  presets: [basePresets],
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './node_modules/@ui-guideline/ui/src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [],
-} satisfies Config;
