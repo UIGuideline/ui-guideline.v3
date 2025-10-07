53a3ddea26ab2398e47066f63d56ed8b904558ef
serudda <sergioruizdavila@gmail.com>
2025-10-07 09:49:30 -0500

    refactor: include side-ui lib file to be mapped by TailwindCSS

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,astro}', './node_modules/@ui-guideline/ui/src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,astro}'],
+
+  plugins: [],
 } satisfies Config;
