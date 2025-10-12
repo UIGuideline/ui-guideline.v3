f4b49627b66d5edc69ffdc8e84ee1ca04e31b30b
serudda <sergioruizdavila@gmail.com>
2025-10-12 18:14:42 -0500

    feat: add camelToKebab helper function and integrate it into TOC generation

--- apps/web/src/lib/helpers.ts
+++ apps/web/src/lib/helpers.ts
@@ -8,3 +8,15 @@
 export const normalize = (path: string) => {
   return path.replaceAll('\\', '/');
 };
+
+/**
+ * Convert a camelCase string to kebab-case.
+ *
+ * @function
+ * @param {string} str - string to convert.
+ * @returns {string} kebab-case string.
+ * @example camelToKebab('figmaKits') // 'figma-kits'
+ */
+export const camelToKebab = (str: string): string => {
+  return str.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
+};
