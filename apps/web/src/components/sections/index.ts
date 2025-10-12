0b21d63c459fc1ec3e656e55844b11492a43c374
serudda <sergioruizdavila@gmail.com>
2025-10-12 17:56:00 -0500

    feat: add ComponentTableOfContents component and update imports

--- apps/web/src/components/sections/index.ts
+++ apps/web/src/components/sections/index.ts
@@ -1,6 +1,5 @@
 export * from './Accessibility';
 export * from './Anatomy';
-export * from './ComponentTableOfContents';
 export * from './FigmaKits';
 export * from './Kpis';
 export * from './Overview';
