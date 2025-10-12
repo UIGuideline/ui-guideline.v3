e13f0d3d4459a543995aa77de6d97c986be41654
serudda <sergioruizdavila@gmail.com>
2025-10-12 18:18:23 -0500

    feat: create theme for TableOfContents item styles and refactor Item component to use it

--- apps/web/src/components/ui/TableOfContents/internals/Item.tsx
+++ apps/web/src/components/ui/TableOfContents/internals/Item.tsx
@@ -1,31 +1,7 @@
 import * as React from 'react';
 import { useTableOfContentsContext } from '../TableOfContents';
-import { tv, type VariantProps } from 'tailwind-variants';
-
-const item = tv({
-  base: [
-    'cursor-pointer',
-    'transition-colors duration-100 ease-out',
-    'text-neutral-400',
-    'font-light',
-    'border-l border-transparent hover:border-neutral-300 hover:text-neutral-50',
-  ],
-  variants: {
-    level: {
-      0: '',
-      1: 'pl-3',
-      2: 'pl-6',
-      3: 'pl-9',
-    },
-    isActive: {
-      true: 'border-amber-200 text-neutral-50 hover:border-amber-200',
-    },
-    defaultVariants: {
-      level: 0,
-      isActive: false,
-    },
-  },
-});
+import { item } from '../theme';
+import { type VariantProps } from 'tailwind-variants';
 
 export type ItemProps = React.ComponentPropsWithoutRef<'li'> &
   VariantProps<typeof item> & {
