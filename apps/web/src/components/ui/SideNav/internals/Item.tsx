107abe1e3c64bd4183427542ac12a1d1da8de377
serudda <sergioruizdavila@gmail.com>
2025-10-12 18:25:32 -0500

    refactor: simplify SideNav component structure and enhance styling with active state management

--- apps/web/src/components/ui/SideNav/internals/Item.tsx
+++ apps/web/src/components/ui/SideNav/internals/Item.tsx
@@ -1,50 +1,15 @@
 import * as React from 'react';
-import { tv, type VariantProps } from 'tailwind-variants';
 
-const item = tv({
-  base: [
-    'cursor-pointer',
-    'transition-colors duration-400 ease-out',
-    'text-gray-400',
-    'font-light',
-    'rounded-r-sm p-1.5',
-    'border-l border-transparent hover:border-gray-300 hover:bg-gray-900 hover:text-gray-50',
-  ],
-  variants: {
-    level: {
-      0: '',
-      1: 'pl-3',
-      2: 'pl-6',
-      3: 'pl-9',
-    },
-    isActive: {
-      true: 'border-amber-200 text-gray-50 hover:border-amber-200',
-    },
-    defaultVariants: {
-      level: 0,
-      isActive: false,
-    },
-  },
-});
+export type ItemProps = React.ComponentPropsWithoutRef<'li'> & {
+  /**
+   * The children of the item.
+   */
+  children: React.ReactNode;
+};
 
-export type ItemProps = React.ComponentPropsWithoutRef<'li'> &
-  VariantProps<typeof item> & {
-    /**
-     * The children of the item.
-     */
-    children: React.ReactNode;
-
-    /**
-     * The depth of the item in the tree.
-     */
-    level: 0 | 1 | 2 | 3;
-  };
-
-export const Item = React.forwardRef<HTMLLIElement, ItemProps>(({ className, level = 0, children, ...props }, ref) => {
-  const classes = item({ level, className });
-
+export const Item = React.forwardRef<HTMLLIElement, ItemProps>(({ className, children, ...props }, ref) => {
   return (
-    <li ref={ref} className={classes} {...props}>
+    <li ref={ref} className={className} {...props}>
       {children}
     </li>
   );
