2fa66f2d81d4c6780e2139b91b07f4a81c869d07
serudda <sergioruizdavila@gmail.com>
2025-10-21 04:37:31 -0500

    feat: enhance Props component with expanded view mode and refactor ToggleGroup for improved usability

--- apps/web/src/components/sections/Props.tsx
+++ apps/web/src/components/sections/Props.tsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import { PropsList, PropsTable } from '@composed';
 import type { PropsData } from '@lib';
 import { ToggleGroup, ToggleGroupSelection } from '@ui';
-import { List, Table } from 'lucide-react';
+import { Menu, Rows3, Table } from 'lucide-react';
 
 const ViewMode = {
   list: 'list',
+  expanded: 'expanded',
   table: 'table',
 } as const;
 type ViewModeType = (typeof ViewMode)[keyof typeof ViewMode];
@@ -21,8 +22,22 @@ export const Props = ({ data = [] }: PropsSectionProps) => {
   const [viewMode, setViewMode] = useState<ViewModeType>(ViewMode.table);
 
   const handleViewChange = (value: string) => {
-    console.log('value', value);
+    if (!value) {
+      setViewMode((prev) => prev);
+      return;
+    }
     setViewMode(value as ViewModeType);
+  };
+
+  const renderView = () => {
+    switch (viewMode) {
+      case ViewMode.table:
+        return <PropsTable data={data} className="overflow-x-auto" />;
+      case ViewMode.expanded:
+        return <PropsList data={data} areExpanded className="w-full" />;
+      case ViewMode.list:
+        return <PropsList data={data} className="w-full" />;
+    }
   };
 
   return (
@@ -38,21 +53,20 @@ export const Props = ({ data = [] }: PropsSectionProps) => {
           onValueChange={handleViewChange}
         >
           <ToggleGroup.Item value={ViewMode.list} aria-label="List view">
-            <List className="size-4" />
+            <Menu className="size-4" />
           </ToggleGroup.Item>
+
+          <ToggleGroup.Item value={ViewMode.expanded} aria-label="Expanded view">
+            <Rows3 className="size-4" />
+          </ToggleGroup.Item>
+
           <ToggleGroup.Item value={ViewMode.table} aria-label="Table view">
             <Table className="size-4" />
           </ToggleGroup.Item>
         </ToggleGroup>
       </div>
 
-      <div className="flex overflow-scroll border border-gray-800 rounded-lg">
-        {viewMode === ViewMode.table ? (
-          <PropsTable className="overflow-x-auto" data={data} />
-        ) : (
-          <PropsList className="w-full" data={data} />
-        )}
-      </div>
+      <div className="flex overflow-scroll border border-gray-800 rounded-lg">{renderView()}</div>
     </section>
   );
 };
--- apps/web/src/components/ui/ToggleGroup/ToggleGroup.tsx
+++ apps/web/src/components/ui/ToggleGroup/ToggleGroup.tsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import type { item as itemClasses } from './internal';
 import { Item } from './internal';
 import type { ToggleGroupSelection } from './types';
-import { ItemSize, ItemVariant } from './types';
+import { ItemSize } from './types';
 import type { ToggleGroupSingleProps } from '@radix-ui/react-toggle-group';
 import { Root } from '@radix-ui/react-toggle-group';
 import { tv, type VariantProps } from 'tailwind-variants';
 
 export const ToggleGroupContext = React.createContext<VariantProps<typeof itemClasses>>({
   size: ItemSize.base,
-  variant: ItemVariant.neutral,
 });
 
 export type ToggleGroupProps = Omit<ToggleGroupSingleProps, 'type'> &
@@ -46,13 +45,13 @@ const toggleGroup = tv({
 });
 
 export const ToggleGroup = React.forwardRef<HTMLDivElement, ToggleGroupProps>(
-  ({ className, type, variant, size = ItemSize.base, children, isFullWidth = false, ...props }, ref) => {
+  ({ className, type, size = ItemSize.base, children, isFullWidth = false, ...props }, ref) => {
     const classes = toggleGroup({ className, size, isFullWidth });
 
     return (
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       <Root ref={ref} {...props} className={classes} type={type as any}>
-        <ToggleGroupContext.Provider value={{ variant, size }}>{children}</ToggleGroupContext.Provider>
+        <ToggleGroupContext.Provider value={{ size }}>{children}</ToggleGroupContext.Provider>
       </Root>
     );
   },
