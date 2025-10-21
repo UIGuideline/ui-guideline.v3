fba0a1f63053986f8d9f42c8cf9aba26396937bd
serudda <sergioruizdavila@gmail.com>
2025-10-21 03:50:07 -0500

    feat: add PropsList and PropsItem components for improved props visualization and interaction

--- apps/web/src/components/sections/Props.tsx
+++ apps/web/src/components/sections/Props.tsx
@@ -1,7 +1,14 @@
-import { PropsTable } from '@composed';
+import { useState } from 'react';
+import { PropsList, PropsTable } from '@composed';
 import type { PropsData } from '@lib';
 import { ToggleGroup, ToggleGroupSelection } from '@ui';
-import { Menu, Rows3, Table } from 'lucide-react';
+import { List, Table } from 'lucide-react';
+
+const ViewMode = {
+  list: 'list',
+  table: 'table',
+} as const;
+type ViewModeType = (typeof ViewMode)[keyof typeof ViewMode];
 
 export interface PropsSectionProps {
   /**
@@ -11,27 +18,40 @@ export interface PropsSectionProps {
 }
 
 export const Props = ({ data = [] }: PropsSectionProps) => {
+  const [viewMode, setViewMode] = useState<ViewModeType>(ViewMode.table);
+
+  const handleViewChange = (value: string) => {
+    console.log('value', value);
+    setViewMode(value as ViewModeType);
+  };
+
   return (
     <section className="flex flex-col gap-3">
       <div className="flex gap-2">
         <h1 id="props" className="text-2xl text-gray-200 ml-1">
           Props
         </h1>
-        <ToggleGroup type={ToggleGroupSelection.single} className="ml-auto" defaultValue="1">
-          <ToggleGroup.Item value="1" aria-label="Item 1">
-            <Menu className="size-4" />
+        <ToggleGroup
+          type={ToggleGroupSelection.single}
+          className="ml-auto"
+          defaultValue={ViewMode.table}
+          onValueChange={handleViewChange}
+        >
+          <ToggleGroup.Item value={ViewMode.list} aria-label="List view">
+            <List className="size-4" />
           </ToggleGroup.Item>
-          <ToggleGroup.Item value="2" aria-label="Item 2">
-            <Rows3 className="size-4" />
-          </ToggleGroup.Item>
-          <ToggleGroup.Item value="3" aria-label="Item 3">
+          <ToggleGroup.Item value={ViewMode.table} aria-label="Table view">
             <Table className="size-4" />
           </ToggleGroup.Item>
         </ToggleGroup>
       </div>
 
       <div className="flex overflow-scroll border border-gray-800 rounded-lg">
-        <PropsTable className="overflow-x-auto" data={data} />
+        {viewMode === ViewMode.table ? (
+          <PropsTable className="overflow-x-auto" data={data} />
+        ) : (
+          <PropsList className="w-full" data={data} />
+        )}
       </div>
     </section>
   );
