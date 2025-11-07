87daa98626b9bb5097564d7d255686469359bb2b
serudda <sergioruizdavila@gmail.com>
2025-11-07 18:44:58 -0500

    feat: add CodeBlock and Button components with customizable styles and integrate into Anatomy section

---
+++ apps/web/src/components/ui/Button/theme.ts
@@ -0,0 +1,72 @@
+import { ButtonAppearance, ButtonSize, ButtonVariant } from './types';
+import { tv } from 'tailwind-variants';
+
+export const theme = tv({
+  variants: {
+    appearance: {
+      [ButtonAppearance.contained]: '',
+      [ButtonAppearance.outlined]: '',
+      [ButtonAppearance.ghost]: '',
+    },
+    isFullWidth: {
+      true: 'w-full',
+    },
+    size: {
+      [ButtonSize.sm]: 'h-6 rounded px-2 py-2 text-xs',
+      [ButtonSize.base]: 'h-8 rounded px-3.5 text-sm',
+      [ButtonSize.lg]: 'h-10 rounded px-4 text-base',
+    },
+    variant: {
+      [ButtonVariant.primary]: '',
+      [ButtonVariant.neutral]: '',
+      [ButtonVariant.destructive]: '',
+    },
+  },
+  compoundVariants: [
+    {
+      appearance: ButtonAppearance.contained,
+      variant: ButtonVariant.primary,
+      className: ['border-primary-400/20 border', 'bg-primary-500 hover:bg-primary-400/70', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.contained,
+      variant: ButtonVariant.neutral,
+      className: ['border border-gray-700/20', 'bg-gray-800 hover:bg-gray-700/70', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.contained,
+      variant: ButtonVariant.destructive,
+      className: ['border border-red-700/20', 'bg-red-600 hover:bg-red-500/80', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.outlined,
+      variant: ButtonVariant.primary,
+      className: ['border-primary-600/60 border', 'hover:border-primary-400/20 hover:bg-primary-500', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.outlined,
+      variant: ButtonVariant.neutral,
+      className: ['border border-gray-700/60', 'hover:border-gray-700/20 hover:bg-gray-800', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.outlined,
+      variant: ButtonVariant.destructive,
+      className: ['border border-red-700/60', 'hover:border-red-700/20 hover:bg-red-600', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.ghost,
+      variant: ButtonVariant.primary,
+      className: ['hover:bg-primary-500', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.ghost,
+      variant: ButtonVariant.neutral,
+      className: ['hover:bg-gray-800', 'text-white'],
+    },
+    {
+      appearance: ButtonAppearance.ghost,
+      variant: ButtonVariant.destructive,
+      className: ['hover:bg-red-600', 'text-white'],
+    },
+  ],
+});
