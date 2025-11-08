760ba2857341b2c94779cfb3e377297506553fd4
serudda <sergioruizdavila@gmail.com>
2025-11-08 11:48:04 -0500

    refactor: clean code on CodeBlock component

--- apps/web/src/components/ui/CodeBlock/internal/Filename.tsx
+++ apps/web/src/components/ui/CodeBlock/internal/Filename.tsx
@@ -3,26 +3,54 @@ import { useContext } from 'react';
 import { CodeBlockContext } from '../CodeBlock';
 import { filenameIconMap } from './filenameIconMap';
 import type { IconType } from 'react-icons';
+import { tv, type VariantProps } from 'tailwind-variants';
 
-export type FilenameProps = HTMLAttributes<HTMLDivElement> & {
-  icon?: IconType;
-  value?: string;
-};
+const filename = tv({
+  base: 'flex items-center gap-2 bg-secondary px-4 py-1.5 text-muted-foreground text-xs',
+});
 
-export const Filename = ({ icon, value, children, ...props }: FilenameProps) => {
+const icon = tv({
+  base: 'h-4 w-4 shrink-0',
+});
+
+const label = tv({
+  base: 'flex-1 truncate',
+});
+
+export type FilenameProps = HTMLAttributes<HTMLDivElement> &
+  VariantProps<typeof filename> & {
+    /**
+     * The icon to display for the filename.
+     */
+    icon?: IconType;
+
+    /**
+     * The value of the filename.
+     */
+    value?: string;
+  };
+
+export const Filename = ({ icon: iconProp, value, children, className, ...props }: FilenameProps) => {
+  const classes = {
+    filename: filename({ className }),
+    icon: icon(),
+    label: label(),
+  };
+
   const { value: activeValue } = useContext(CodeBlockContext);
+
   const defaultIcon = Object.entries(filenameIconMap).find(([pattern]) => {
     const regex = new RegExp(`^${pattern.replace(/\\/g, '\\\\').replace(/\./g, '\\.').replace(/\*/g, '.*')}$`);
     return regex.test(children as string);
   })?.[1];
-  const Icon = icon ?? defaultIcon;
+  const Icon = iconProp ?? defaultIcon;
 
   if (value !== activeValue) return null;
 
   return (
-    <div className="flex items-center gap-2 bg-secondary px-4 py-1.5 text-muted-foreground text-xs" {...props}>
-      {Icon && <Icon className="h-4 w-4 shrink-0" />}
-      <span className="flex-1 truncate">{children}</span>
+    <div className={classes.filename} {...props}>
+      {Icon && <Icon className={classes.icon} />}
+      <span className={classes.label}>{children}</span>
     </div>
   );
 };
