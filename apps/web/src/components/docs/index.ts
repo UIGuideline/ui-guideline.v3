652f416eaba9035034cc4ef4eb2a81d8b73cf818
serudda <sergioruizdavila@gmail.com>
2025-11-16 11:56:40 -0500

    feat: introduce documentation components and layout for improved overview section

---
+++ apps/web/src/components/docs/index.ts
@@ -0,0 +1,1 @@
+export { docsComponents } from './MDXComponents';
--- apps/web/src/components/layouts/index.ts
+++ apps/web/src/components/layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as BaseLayout } from './BaseLayout.astro';
 export { default as ComponentDetailLayout } from './ComponentDetailLayout.astro';
+export { default as DocsLayout } from './DocsLayout.astro';
