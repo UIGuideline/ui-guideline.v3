d6b45d1178d7ddf409cd3fd69dca249996eb1985
serudda <sergioruizdavila@gmail.com>
2025-11-16 14:35:12 -0500

    feat: enhance component detail layout with overview section and remove deprecated docs layout

--- apps/web/src/components/layouts/index.ts
+++ apps/web/src/components/layouts/index.ts
@@ -1,3 +1,2 @@
 export { default as BaseLayout } from './base-layout.astro';
 export { default as ComponentDetailLayout } from './component-detail-layout.astro';
-export { default as DocsLayout } from './docs-layout.astro';
