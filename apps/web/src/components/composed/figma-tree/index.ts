27b2ab3559e4e2884bef884965245381c7d52c84
serudda <sergioruizdavila@gmail.com>
2025-12-08 11:32:20 -0500

    refactor: migrate Figma icon and node data management from `figma-tree-icons` to `figma-node-catalog`

--- apps/web/src/components/composed/figma-tree/index.ts
+++ apps/web/src/components/composed/figma-tree/index.ts
@@ -1,3 +1,1 @@
 export { FigmaTree, useFigmaTree } from './figma-tree';
-export { DEFAULT_FIGMA_ICON, FIGMA_TREE_ICONS, getFigmaIconForType } from './figma-tree-icons';
-export type { IconComponent } from './figma-tree-icons';
