c0a4a79a492a5c85219f95e3227f55d6b23eb016
serudda <sergioruizdavila@gmail.com>
2025-12-08 13:22:05 -0500

    refactor: rename `props` collection to `codeProps`, restructure prop data to be component-centric

--- apps/web/src/content/components/types.ts
+++ apps/web/src/content/components/types.ts
@@ -2,14 +2,14 @@ import type { CollectionEntry } from 'astro:content';
 
 // Types derived from Astro Collections
 export type OverviewEntry = CollectionEntry<'overview'>;
-export type PropsEntry = CollectionEntry<'props'>;
+export type CodePropsEntry = CollectionEntry<'codeProps'>;
 export type AnatomyEntry = CollectionEntry<'anatomy'>;
 export type CodeAnatomyEntry = CollectionEntry<'codeAnatomy'>;
 export type DesignLayersEntry = CollectionEntry<'designLayers'>;
 
 // Extracted data types for easier use in components
 export type OverviewData = OverviewEntry['data'];
-export type PropsData = PropsEntry['data'];
+export type CodePropsData = CodePropsEntry['data'];
 export type AnatomyData = AnatomyEntry['data'];
 export type CodeAnatomyData = CodeAnatomyEntry['data'];
 export type DesignLayersData = DesignLayersEntry['data'];
